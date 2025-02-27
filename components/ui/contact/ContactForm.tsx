8e90b39d2a85d7e3f70916b5fe26fc5338da301f
Anna <annarosewain9@gmail.com>
2025-02-27 07:53:20 +0000

    Deleted unnecessary files for debugging

--- components/ui/contact/ContactForm.tsx
+++ components/ui/contact/ContactForm.tsx
@@ -1,20 +1,5 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
 import {
   Card,
   CardContent,
@@ -22,26 +7,9 @@ import {
   CardHeader,
   CardTitle,
 } from "../card";
-import { contactSchema } from "@/lib/schemas/contactSchema";
-import { send } from "@/api/contact";
 
 const ContactForm = () => {
   // 1. Define your form.
-  const form = useForm<z.infer<typeof contactSchema>>({
-    resolver: zodResolver(contactSchema),
-    defaultValues: {
-      firstName: "",
-      lastName: "",
-      email: "",
-      message: "",
-    },
-  });
-  // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof contactSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    send(values);
-  }
 
   return (
     <Card className="mx-auto max-w-md text-left border-hidden shadow-none">
@@ -58,83 +26,7 @@ const ContactForm = () => {
           </p>
         </CardDescription>
       </CardHeader>
-      <CardContent className="p-1">
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <div className="grid grid-cols-2 gap-4">
-              <div className="space-y-2">
-                <FormField
-                  control={form.control}
-                  name="firstName"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>First Name</FormLabel>
-                      <FormControl>
-                        <Input placeholder="Your first name" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-              <div className="space-y-1">
-                <FormField
-                  control={form.control}
-                  name="lastName"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Last Name</FormLabel>
-                      <FormControl>
-                        <Input placeholder="Your last name" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-            </div>
-            <div className="space-y-1">
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Your Email" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-
-            <div className="space-y-1">
-              <FormField
-                control={form.control}
-                name="message"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Message</FormLabel>
-                    <FormControl>
-                      <Textarea
-                        id="message"
-                        placeholder="Type in your message here"
-                        className="min-h-[120px]"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-            <Button type="submit" variant="primary" className="w-full">
-              Submit
-            </Button>
-          </form>
-        </Form>
-      </CardContent>
+      <CardContent className="p-1"></CardContent>
     </Card>
   );
 };
